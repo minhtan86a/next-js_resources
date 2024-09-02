a0c6f50fe6359100ae6126abb4fde065623cadd5
TanNguyen <minhtan86a@gmail.com>
2024-09-02 20:47:18 +0700

    Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="text-white-800 flex-between body-text w-full gap-y-10 border-t border-black-400 bg-black-100 px-20 py-1 max-md:flex-col">
+      <p>Copyright @ 2023 JS Mastery Pro | All Rights Reserved</p>
+      <div className="flex gap-x-9">
+        <Link href="/terms-of-use">Terms & Conditions</Link>
+        <Link href="/privacy-policy">Privacy Policy</Link>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
