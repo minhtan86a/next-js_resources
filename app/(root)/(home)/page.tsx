b81d444a6837f18965c13a80adf1d4a9a39209cd
TanNguyen <minhtan86a@gmail.com>
2024-09-02 20:59:51 +0700

    Home Page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,19 @@
+import SearchForm from "@/components/SearchForm";
 import React from "react";
 
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
+      <section className="nav-padding w-full">
+        <div className="flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center">
+          <h1 className="sm:heading1 heading2 mb-6 text-center text-white">
+            Javascript Mastery Resources
+          </h1>
+        </div>
+        <SearchForm />
+      </section>
+    </main>
+  );
 };
 
 export default Page;
